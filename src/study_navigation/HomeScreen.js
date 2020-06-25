b2b3b3a5cd9ed3f21f5b65bceeec056f4220bab2
zhaowenliang <zwldh123@163.com>
2020-06-25 19:16:00 +0800

    增加一个控制显示隐藏View的案例

--- src/study_navigation/HomeScreen.js
+++ src/study_navigation/HomeScreen.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar, Text, View, Button } from 'react-native';
 
 
 export default class HomeScreen extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { showWarning: true };
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
 
   render() {
     return (
       <>
         <StatusBar backgroundColor={'green'} barStyle={'light-content'} />
 
-        <Text>This is HomeScreen</Text>
+        <WarningBanner warn={this.state.showWarning} />
+
+        <Button onPress={this.handleToggleClick} title={this.state.showWarning ? 'Hide' : 'Show'} />
 
         <View style={{ height: 10 }} />
       </>
     );
   }
 
+}
+
+// custom view
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <View>
+      <Text style={{ fontSize: 40, color: 'red', alignSelf: 'center' }}>Warning!</Text>
+    </View>
+  );
 }
